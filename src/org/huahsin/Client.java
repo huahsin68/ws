87878fc199bcc643f3da73608ca8a07b13a0520f
Kok Hoe Loh <Kok.Hoe.Loh@PRU.intranet.asia>
2015-12-28 09:58:22 +0800

    1. Added support for asynchronous web service.

--- src/org/huahsin/Client.java
+++ src/org/huahsin/Client.java
@@ -2,20 +2,56 @@ package org.huahsin;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.ExecutionException;
 
 import javax.xml.namespace.QName;
-import javax.xml.ws.Service;
+import javax.xml.ws.AsyncHandler;
+import javax.xml.ws.Response;
 
-import org.huahsin.webmethod.IHelloWorld;
+import org.huahsin.async.HelloWorldImplService;
+import org.huahsin.async.IHelloWorld;
+import org.huahsin.async.SayHelloWorldResponse;
 
 public class Client {
 
-	public static void main(String[] args) throws MalformedURLException {
-		URL url = new URL("http://localhost:8080/ws1?wsdl");
+	static private double rate = 0.0;
+	static private String msg = "";
+	
+	public static void main(String[] args) throws InterruptedException, MalformedURLException, ExecutionException {
+		Client e = new Client();
 		
+		System.out.println("before: " + msg);
+		e.sayHelloWorldMethod();
+		Thread.sleep(1000);
+		System.out.println("after: " + msg);
+	}
+	
+	private void sayHelloWorldMethod() throws MalformedURLException, InterruptedException, ExecutionException {
+		URL url = new URL("http://localhost:8080/ws1?wsdl");
 		QName qname = new QName("http://webmethod.huahsin.org/", "HelloWorldImplService");
-		Service service = Service.create(url, qname);
-		IHelloWorld hw = service.getPort(IHelloWorld.class);
-		System.out.println(hw.sayHelloWorld());
+		HelloWorldImplService service = new HelloWorldImplService(url, qname);
+		IHelloWorld hello = service.getHelloWorldImplPort();
+		
+		Response res = hello.sayHelloWorldAsync();
+		SayHelloWorldResponse output = (SayHelloWorldResponse) res.get();
+		output.getReturn();
+		
+		hello.sayHelloWorldAsync(new AsyncHandler() {
+
+			@Override
+			public void handleResponse(Response res) {
+				try {
+					setMessage(((SayHelloWorldResponse)res.get()).getReturn());
+				} catch (InterruptedException | ExecutionException e) {
+					e.printStackTrace();
+				}
+			}
+			
+		});
+	}
+
+	private void setMessage(String msg) {
+		this.msg = msg;
+		System.out.println("Set message: " + msg);
 	}
 }
